2dc9183693ffd606ffa6fce516882f9bb3c29b15
Carlos Ramirez <ramirespinoza@gmail.com>
2022-10-05 20:42:36 -0600

    Se arregla en app.blade.php y en guest.blade.php las llamadas al CSS para arreglar problemas en las vista

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,6 +12,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">

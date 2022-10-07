1f628695201de20d24d5e1abd0aa7d4288bb40dd
JulietaPortillo <julie.portillo96@gmail.com>
2022-10-06 22:34:30 -0600

    create controller

---
+++ database/migrations/2022_10_06_075940_create_professors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfessorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professor', function (Blueprint $table) {
+            $table->id();
+            $table->integer('dpi');
+            $table->string('name', 50);
+            $table->string('last_name', 50);
+ /**pendiente agregar relaciòn con escuela */
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professor');
+    }
+}

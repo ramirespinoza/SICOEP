69673340cb8f5bb6b80ec2b72b96ab66d2d94f37
Carlos Ramirez <ramirespinoza@gmail.com>
2022-10-07 21:47:49 -0600

    Se agrea el consultar y crear estudiantes en el controller para la API

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
@@ -13,7 +14,8 @@ class StudentController extends Controller
      */
     public function index()
     {
-        //
+        $students = Student::all();
+        return $students;
     }
 
     /**
@@ -34,7 +36,24 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $customer = new Student();
+
+        $customer->personal_code = $request->get('personal_code');
+        $customer->name = $request->get('name');
+        $customer->last_name = $request->get('last_name');
+        $customer->grade_id = $request->get('grade_id');
+        $customer->section = $request->get('section');
+        $customer->birth_date = $request->get('birth_date');
+        $customer->identification_document = $request->
+            get('identification_document');
+        $customer->identification_document_number = $request->
+            get('identification_document_number');
+        $customer->class_schedule_id = $request->get('class_schedule_id');
+        $customer->professor_dpi = $request->get('professor_dpi');
+        $customer->tutelary_name = $request->get('tutelary_name');
+        $customer->tutelary_dpi = $request->get('tutelary_dpi');
+
+        $customer->save();
     }
 
     /**
--- database/migrations/2022_10_08_021712_create_student_table.php
+++ database/migrations/2022_10_08_021712_create_student_table.php
@@ -14,21 +14,21 @@ class CreateStudentTable extends Migration
     public function up()
     {
         Schema::create('student', function (Blueprint $table) {
-            $table->string('personal_code', 10)->unique()->primary();
-            $table->string('name', 50);
-            $table->string('last_name', 50);
+            $table->string('personal_code', '10 char')->unique()->primary();
+            $table->string('name', '50 char');
+            $table->string('last_name', '50 char');
             $table->unsignedBigInteger('grade_id');
-            $table->char('section', 1);
-            $table->string('identification_document', 5);
+            $table->char('section', '1 char');
+            $table->string('identification_document', '5 char');
             $table->unsignedBigInteger('identification_document_number');
             $table->unsignedBigInteger('class_schedule_id');
             $table->unsignedBigInteger('professor_dpi');
-            $table->string('tutelary_name', 50);
+            $table->string('tutelary_name', '50 char');
             $table->unsignedBigInteger('tutelary_dpi');
             $table->timestamps();
 
             // foreing keys
-            $table->foreign('professor_dpi')->references('dpi')->on('professor');
+            //$table->foreign('professor_dpi')->references('dpi')->on('professor');
         });
     }
 

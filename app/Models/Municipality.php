b2acb707bd6a0d05725565226e3c9f28542b4806
Carlos Ramirez <ramirespinoza@gmail.com>
2022-10-09 23:34:29 -0600

    Se crea las migraciones y modelos para las tablas de Departamento y Municipalidad

---
+++ app/Models/Municipality.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Municipality extends Model
+{
+    use HasFactory;
+
+    protected $table = 'municipality';
+
+    protected $fillable = [
+        'name'
+    ];
+}

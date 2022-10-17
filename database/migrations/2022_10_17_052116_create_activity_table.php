5fbd59a6db08b36a54df63f85d7acdd471698d12
Carlos Ramirez <ramirespinoza@gmail.com>
2022-10-16 23:57:48 -0600

    Se crea el modelo y la migración para la tabla de Actividades

---
+++ database/migrations/2022_10_17_052116_create_activity_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', '50 CHAR');
+            $table->string('description', '100 CHAR');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activity');
+    }
+}
